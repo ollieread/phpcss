b6643d2671e9683550b869dc5ed0770dadd727ce
Ollie Read <code@ollieread.com>
2025-09-18 14:16:15 +0100

    refactor(syntax@3): Allow for parse error exceptions to have a token attached

--- modules/syntax-l3/src/Tokenizer/Exceptions/ParseErrorException.php
+++ modules/syntax-l3/src/Tokenizer/Exceptions/ParseErrorException.php
@@ -3,12 +3,36 @@ declare(strict_types=1);
 
 namespace PhpCss\Modules\Syntax\L3\Tokenizer\Exceptions;
 
-use Exception;
+use PhpCss\Modules\Syntax\L3\Tokenizer\Token;
+use RuntimeException;
+use Throwable;
 
-final class ParseErrorException extends Exception
+final class ParseErrorException extends RuntimeException
 {
-    public static function make(string $message): self
+    public static function make(string $message, ?Token $token = null): self
     {
-        return new self('Parse Error: ' . $message);
+        return new self(
+            message: 'Parse Error: ' . $message,
+            token  : $token
+        );
+    }
+
+    protected ?Token $token;
+
+    public function __construct(string $message = "", int $code = 0, ?Token $token = null, ?Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+
+        $this->token = $token;
+    }
+
+    public function hasToken(): bool
+    {
+        return $this->token !== null;
+    }
+
+    public function getToken(): ?Token
+    {
+        return $this->token;
     }
 }
